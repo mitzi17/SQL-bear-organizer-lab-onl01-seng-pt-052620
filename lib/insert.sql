0671a286e106379ba030eeab29a366f6e59e0803
Mitzi Estrada <m_estrada17@live.com>
2020-07-23 02:46:48 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', '');
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy',
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha',
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown',
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'brown', 'calm', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 5, 'M', 'black', 'aggressive', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 5, 'F', 'brown', 'friendly', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa',
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch',
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy',
