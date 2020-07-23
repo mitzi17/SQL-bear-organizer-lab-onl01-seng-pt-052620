ccddc5ee7007b48420899bb6da54032efd9e537b
Mitzi Estrada <m_estrada17@live.com>
2020-07-23 02:49:31 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'brown', 'calm', 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 5, 'M', 'black', 'aggressive', 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 5, 'F', 'brown', 'friendly', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa',
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch',
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 8, 'M', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 4, 'F', 'black', 'friendly', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 3, 'M', 'brown', 'aggressive', 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy',
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL,
